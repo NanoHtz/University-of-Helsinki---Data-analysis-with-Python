e3d3f791b67afce26d3d6726850463ee690c5d22
NanoHtz <fgalvez.g26@gmail.com>
2025-03-14 14:24:09 +0100

    Add excersise 8

---
+++ Chapter 2/2.Python (Part 2)/08-prepend.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class Prepend(object):
+	"""Asi se crea una clase, que debe estar basada en una clase previa(object)"""
+	def __init__ (self, start):
+		"""Inicializamos la clase, el primer parametro debe ser siempre self, lo demas seran argumentos, que querras que el usuario pase al objeto"""
+		self.start = start
+
+	def write(self, s):
+		"""Escribe la concatenacion de la cadena inicial con lo introducido en el método"""
+		print(self.start + s)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
